1d300e3562bc5a07197305c42b0de9e2b36ed1db
andreychernishov <andruha46@gmail.com>
2022-02-22 16:26:39 +0200

    ddd

---
+++ app/src/main/java/com/example/calculator/CalculatorModel.java
@@ -0,0 +1,143 @@
+package com.example.calculator;
+public class CalculatorModel {
+    private int firstArg;
+    private int secondArg;
+    private StringBuilder inputStr = new StringBuilder();
+    private int actionSelected;
+    private State state;
+
+    private enum State {
+        firstArgInput,
+        operationSelected,
+        secondArgInput,
+        resultShow
+    }
+
+    public CalculatorModel() {
+        state = State.firstArgInput;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+        if (state == State.resultShow) {
+            state = State.firstArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (state == State.operationSelected) {
+            state = State.secondArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (inputStr.length() < 9) {
+            switch (buttonId) {
+                case R.id.button_0:
+                    if (inputStr.length() != 0) {
+                        inputStr.append("0");
+                    }
+                    break;
+                case R.id.button_1:
+                    inputStr.append("1");
+                    break;
+                case R.id.button_2:
+                    inputStr.append("2");
+                    break;
+                case R.id.button_3:
+                    inputStr.append("3");
+                    break;
+                case R.id.button_4:
+                    inputStr.append("4");
+                    break;
+                case R.id.button_5:
+                    inputStr.append("5");
+                    break;
+                case R.id.button_6:
+                    inputStr.append("6");
+                    break;
+                case R.id.button_7:
+                    inputStr.append("7");
+                    break;
+                case R.id.button_8:
+                    inputStr.append("8");
+                    break;
+                case R.id.button_9:
+                    inputStr.append("9");
+                    break;
+            }
+        }
+
+    }
+
+    public void onActionPressed(int actionId) {
+        if (actionId == R.id.button_equals && state == State.secondArgInput && inputStr.length() > 0) {
+            secondArg = Integer.parseInt(inputStr.toString());
+            state = State.resultShow;
+            inputStr.setLength(0);
+            switch (actionSelected) {
+                case R.id.button_plus:
+                    inputStr.append(firstArg + secondArg);
+                    break;
+                case R.id.button_minus:
+                    inputStr.append(firstArg - secondArg);
+                    break;
+                case R.id.button_multiply:
+                    inputStr.append(firstArg * secondArg);
+                    break;
+                case R.id.button_divide:
+                    inputStr.append(firstArg / secondArg);
+                    break;
+            }
+
+        } else if (inputStr.length() > 0 && state == State.firstArgInput && actionId != R.id.button_equals) {
+            firstArg = Integer.parseInt(inputStr.toString());
+            state = State.operationSelected;
+            actionSelected = actionId;
+        }
+    }
+
+    public String getText() {
+        StringBuilder str = new StringBuilder();
+        switch (state) {
+            default:
+                return inputStr.toString();
+            case operationSelected:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .toString();
+            case secondArgInput:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(inputStr)
+                        .toString();
+            case resultShow:
+                return str.append(firstArg).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(secondArg)
+                        .append(" = ")
+                        .append(inputStr.toString())
+                        .toString();
+        }
+    }
+
+    private char getOperationChar() {
+        switch (actionSelected) {
+            case R.id.button_plus:
+                return '+';
+            case R.id.button_minus:
+                return '-';
+            case R.id.button_multiply:
+                return '*';
+            case R.id.button_divide:
+            default:
+                return '/';
+
+        }
+    }
+
+    public void reset() {
+        state = State.firstArgInput;
+        inputStr.setLength(0);
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,17 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity {
 
 
-
-import android.widget.Button;
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TextView screen;
-    private String saveScreen = "";
     public static final String SAVE = "SCREEN";
+    private CalculatorModel calculator;
+    private TextView tvResult;
 
 
     @Override
@@ -26,147 +21,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
+        int[] numberIds = new int[] {
+                R.id.button_0,
+                R.id.button_1,
+                R.id.button_2,
+                R.id.button_3,
+                R.id.button_4,
+                R.id.button_5,
+                R.id.button_6,
+                R.id.button_7,
+                R.id.button_8,
+                R.id.button_9
+        };
 
-        if (savedInstanceState != null) {
-            saveScreen = savedInstanceState.getString(SAVE, "");
+        int[] actionsIds = new int[] {
+                R.id.button_plus,
+                R.id.button_minus,
+                R.id.button_multiply,
+                R.id.button_divide,
+                R.id.button_equals
+        };
+
+        tvResult = findViewById(R.id.result_view);
+
+        calculator = new CalculatorModel();
+
+        View.OnClickListener numberButtonClickListener = view -> {
+            calculator.onNumPressed(view.getId());
+            tvResult.setText(calculator.getText());
+        };
+
+        View.OnClickListener actionButtonOnclickListener = view -> {
+            calculator.onActionPressed(view.getId());
+            tvResult.setText(calculator.getText());
+        };
+
+        for (int numberId : numberIds) {
+            findViewById(numberId).setOnClickListener(numberButtonClickListener);
         }
 
-        screen.setText(saveScreen);
-    }
-
-    private void initView() {
-
-        screen = findViewById(R.id.input_view);
-        initClickListener();
-
-    }
-
-
-    private void initClickListener() {
-
-        Button button0 = findViewById(R.id.button_0);
-        Button button1 = findViewById(R.id.button_1);
-        Button button2 = findViewById(R.id.button_2);
-        Button button3 = findViewById(R.id.button_3);
-        Button button4 = findViewById(R.id.button_4);
-        Button button5 = findViewById(R.id.button_5);
-        Button button6 = findViewById(R.id.button_6);
-        Button button7 = findViewById(R.id.button_7);
-        Button button8 = findViewById(R.id.button_8);
-        Button button9 = findViewById(R.id.button_9);
-        Button buttonPoint = findViewById(R.id.button_point);
-        Button buttonPercent = findViewById(R.id.button_percent);
-        Button buttonMultiply = findViewById(R.id.button_multiply);
-        Button buttonDivide = findViewById(R.id.button_divide);
-        Button buttonPlus = findViewById(R.id.button_plus);
-        Button buttonMinus = findViewById(R.id.button_minus);
-        Button buttonEquals = findViewById(R.id.button_equals);
-        Button buttonReset = findViewById(R.id.button_clear);
-
-        button0.setOnClickListener(this);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-        button8.setOnClickListener(this);
-        button9.setOnClickListener(this);
-        buttonPoint.setOnClickListener(this);
-        buttonPercent.setOnClickListener(this);
-        buttonMultiply.setOnClickListener(this);
-        buttonDivide.setOnClickListener(this);
-        buttonPlus.setOnClickListener(this);
-        buttonMinus.setOnClickListener(this);
-        buttonEquals.setOnClickListener(this);
-        buttonReset.setOnClickListener(this);
-
-    }
-
-    private void setText(String s) {
-        saveScreen = saveScreen + s;
-        screen.setText(saveScreen);
-    }
-
-    private void deleteAll(){
-        saveScreen = "";
-        screen.setText(saveScreen);
-    }
-    private void deleteNumber(){
-        saveScreen = saveScreen;
-        screen.setText(screen.length()-1);
-    }
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId()) {
-            case R.id.button_0:
-                setText("0");
-                break;
-            case R.id.button_1:
-                setText("1");
-                break;
-            case R.id.button_2:
-                setText("2");
-                break;
-            case R.id.button_3:
-                setText("3");
-                break;
-            case R.id.button_4:
-                setText("4");
-                break;
-            case R.id.button_5:
-                setText("5");
-                break;
-            case R.id.button_6:
-                setText("6");
-                break;
-            case R.id.button_7:
-                setText("7");
-                break;
-            case R.id.button_8:
-                setText("8");
-                break;
-            case R.id.button_9:
-                setText("9");
-                break;
-            case R.id.button_percent:
-                setText("%");
-                break;
-            case R.id.button_point:
-                setText(".");
-                break;
-            case R.id.button_multiply:
-                setText("*");
-                break;
-            case R.id.button_divide:
-                setText("/");
-                break;
-            case R.id.button_plus:
-                setText("+");
-                break;
-            case R.id.button_minus:
-                setText("-");
-                break;
-
-            case R.id.button_clear:
-                deleteAll();
-                break;
-            case R.id.button_equals:
-                setText("=");
-                break;
-            case R.id.button_delete:
-                deleteNumber();
-                break;
+        for (int actionsId : actionsIds) {
+            findViewById(actionsId).setOnClickListener(actionButtonOnclickListener);
         }
 
+        findViewById(R.id.button_clear).setOnClickListener(view -> {
+            calculator.reset();
+            tvResult.setText(calculator.getText());
+        });
     }
+
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(SAVE, saveScreen);
+        outState.putString(SAVE, String.valueOf(calculator));
     }
 }
